2aac83f2b0470b3dd002712324c33dd9871b334f
saman <saman.nesary@gmail.com>
2023-02-24 00:57:51 +0330

    feat(infra): add infrastructure layer

---
+++ pkg/infrastructure/infrastructure.go
@@ -0,0 +1,44 @@
+package infraContainer
+
+import (
+	"context"
+	"go.uber.org/zap"
+
+	"github.com/samannsr/vending-machine-control-system/pkg/config"
+	echoHttp "github.com/samannsr/vending-machine-control-system/pkg/http/echo"
+	"github.com/samannsr/vending-machine-control-system/pkg/logger"
+)
+
+type IContainer struct {
+	Config         *config.Config
+	Logger         *zap.Logger
+	EchoHttpServer echoHttp.ServerInterface
+}
+
+func NewIC(ctx context.Context) (*IContainer, func(), error) {
+	var downFns []func()
+	down := func() {
+		for _, df := range downFns {
+			df()
+		}
+	}
+
+	echoServerConfig := &echoHttp.ServerConfig{
+		Port:     config.BaseConfig.Http.Port,
+		BasePath: "/api/v1",
+		IsDev:    config.IsDevEnv(),
+	}
+	echoServer := echoHttp.NewServer(echoServerConfig)
+	echoServer.SetupDefaultMiddlewares()
+	downFns = append(downFns, func() {
+		_ = echoServer.GracefulShutdown(ctx)
+	})
+
+	ic := &IContainer{
+		Config:         config.BaseConfig,
+		Logger:         logger.Zap,
+		EchoHttpServer: echoServer,
+	}
+
+	return ic, down, nil
+}
