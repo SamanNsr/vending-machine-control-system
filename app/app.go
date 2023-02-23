a8e9e7cae8feab1af3e8bbcaa06028613cbe0778
saman <saman.nesary@gmail.com>
2023-02-24 01:01:32 +0330

    feat(app): config app layer

---
+++ app/app.go
@@ -0,0 +1,58 @@
+package app
+
+import (
+	"context"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"github.com/samannsr/vending-machine-control-system/pkg/infrastructure"
+)
+
+type App struct{}
+
+func New() *App {
+	return &App{}
+}
+
+func (a *App) Run() error {
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	ic, infraDown, err := infrastructure.NewIC(ctx)
+	if err != nil {
+		return err
+	}
+	defer infraDown()
+
+	me := configureModule(ctx, ic)
+	if me != nil {
+		return me
+	}
+
+	var serverError error
+	go func() {
+		if err := ic.EchoHttpServer.RunServer(ctx, nil); err != nil {
+			ic.Logger.Sugar().Errorf("(s.RunEchoServer) err: {%v}", err)
+			serverError = err
+			cancel()
+		}
+	}()
+
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, os.Interrupt, syscall.SIGTERM)
+
+	select {
+	case v := <-quit:
+		ic.Logger.Sugar().Errorf("signal.Notify: %v", v)
+	case done := <-ctx.Done():
+		ic.Logger.Sugar().Errorf("ctx.Done: %v", done)
+	}
+
+	ic.Logger.Sugar().Info("Server Exited Properly")
+	return serverError
+}
+
+func configureModule(ctx context.Context, ic *infrastructure.IContainer) error {
+	return nil
+}
--- pkg/infrastructure/infrastructure.go
+++ pkg/infrastructure/infrastructure.go
@@ -1,4 +1,4 @@
-package infraContainer
+package infrastructure
 
 import (
 	"context"
