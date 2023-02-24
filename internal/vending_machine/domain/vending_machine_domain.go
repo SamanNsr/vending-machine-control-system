3f94b333df954ef90aa6a3dd8a62c3618bd10982
saman <saman.nesary@gmail.com>
2023-02-24 12:48:42 +0330

    feat(vm): add business enterprise rules in domain for vending machine

---
+++ internal/vending_machine/domain/vending_machine_domain.go
@@ -0,0 +1,39 @@
+package vendingMachineDomain
+
+import (
+	"context"
+	"github.com/labstack/echo/v4"
+	"time"
+)
+
+type VendingMachine struct {
+	ID         int `json:"id"`
+	Status     string
+	Inventory  *Inventory
+	Coins      int
+	LastAccess time.Time
+}
+
+type Inventory struct {
+	Cola   int
+	Coffee int
+}
+
+type Configurator interface {
+	Configure(ctx context.Context) error
+}
+
+type UseCase interface {
+	InsertCoin(ctx context.Context, vmID int, coin int) error
+	SelectProduct(ctx context.Context, vmID int, productName string) error
+}
+
+type Repository interface {
+	GetVendingMachineByID(ctx context.Context, vmID int) (*VendingMachine, error)
+	UpdateVendingMachine(ctx context.Context, vm *VendingMachine) error
+}
+
+type HttpController interface {
+	InsertCoin(c echo.Context) error
+	SelectProduct(c echo.Context) error
+}
