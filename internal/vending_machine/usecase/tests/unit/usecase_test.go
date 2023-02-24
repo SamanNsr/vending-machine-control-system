56fbca44f17a48e70e668bbddf56978918170e11
saman <saman.nesary@gmail.com>
2023-02-24 22:26:10 +0330

    feat(vm): complete selecting product test

--- internal/vending_machine/usecase/tests/unit/usecase_test.go
+++ internal/vending_machine/usecase/tests/unit/usecase_test.go
@@ -28,7 +28,7 @@ func (suite *testSuite) TestGetVendingMachineByIdSuccess() {
 	vms = append(vms, vendingMachineUseCaseFixtures.VM1, vendingMachineUseCaseFixtures.VM2)
 	uc := vendingMachineUseCase.NewUseCase(vms)
 
-	result, err := uc.GetVendingMachineById(ctx, &vendingMachineDto.InsertCoinRequestDto{
+	result, err := uc.GetVendingMachineById(ctx, &vendingMachineDto.GetVendingMachineByIdRequestDto{
 		MachineID: vendingMachineUseCaseFixtures.VM1.ID,
 	})
 	assert.NoError(suite.T(), err)
@@ -45,7 +45,7 @@ func (suite *testSuite) TestGetVendingMachineByIdFailedMachineNotFound() {
 	vms = append(vms, vendingMachineUseCaseFixtures.VM1, vendingMachineUseCaseFixtures.VM2)
 	uc := vendingMachineUseCase.NewUseCase(vms)
 
-	_, err := uc.GetVendingMachineById(ctx, &vendingMachineDto.InsertCoinRequestDto{
+	_, err := uc.GetVendingMachineById(ctx, &vendingMachineDto.GetVendingMachineByIdRequestDto{
 		MachineID: vendingMachineUseCaseFixtures.VM3.ID,
 	})
 
@@ -101,6 +101,20 @@ func (suite *testSuite) TestInsertCoinFailedMachineNotFound() {
 	assert.True(suite.T(), customError.IsNotFoundError(err))
 }
 
+func (suite *testSuite) TestInsertCoinFailedNotEnoughInventory() {
+	ctx := context.Background()
+	var vms = make([]vendingMachineDomain.VendingMachine, 0)
+	vms = append(vms, vendingMachineUseCaseFixtures.VM6)
+	uc := vendingMachineUseCase.NewUseCase(vms)
+
+	_, err := uc.InsertCoin(ctx, &vendingMachineDto.InsertCoinRequestDto{
+		MachineID: vendingMachineUseCaseFixtures.VM6.ID,
+	})
+
+	assert.Error(suite.T(), err)
+	assert.True(suite.T(), customError.IsBadRequestError(err))
+}
+
 func (suite *testSuite) TestSelectProductSuccess() {
 	// Create a new vending machine with a cola and a coffee
 	ctx := context.Background()
@@ -120,4 +134,99 @@ func (suite *testSuite) TestSelectProductSuccess() {
 		assert.NotNil(suite.T(), result.Message)
 	}
 
+}
+
+func (suite *testSuite) TestSelectProductNotInSelectingState() {
+	ctx := context.Background()
+	var vms = make([]vendingMachineDomain.VendingMachine, 0)
+	vms = append(vms, vendingMachineUseCaseFixtures.VM1, vendingMachineUseCaseFixtures.VM2, vendingMachineUseCaseFixtures.VM3)
+	uc := vendingMachineUseCase.NewUseCase(vms)
+
+	// Attempt to select a product
+	_, err := uc.SelectProduct(ctx, &vendingMachineDto.SelectProductRequestDto{
+		MachineID: vendingMachineUseCaseFixtures.VM1.ID,
+		Product:   vendingMachineConstants.ColaType,
+	})
+
+	assert.Error(suite.T(), err)
+	assert.True(suite.T(), customError.IsBadRequestError(err))
+}
+
+func (suite *testSuite) TestSelectProductInvalidProduct() {
+	ctx := context.Background()
+	var vms = make([]vendingMachineDomain.VendingMachine, 0)
+	vms = append(vms, vendingMachineUseCaseFixtures.VM1, vendingMachineUseCaseFixtures.VM2, vendingMachineUseCaseFixtures.VM3)
+	uc := vendingMachineUseCase.NewUseCase(vms)
+
+	// Attempt to select an invalid product
+	_, err := uc.SelectProduct(ctx, &vendingMachineDto.SelectProductRequestDto{
+		MachineID: vendingMachineUseCaseFixtures.VM3.ID,
+		Product:   "invalid_product",
+	})
+
+	assert.Error(suite.T(), err)
+	assert.True(suite.T(), customError.IsBadRequestError(err))
+}
+
+func (suite *testSuite) TestSelectProductFailedMachineNotFound() {
+	ctx := context.Background()
+	var vms = make([]vendingMachineDomain.VendingMachine, 0)
+	vms = append(vms, vendingMachineUseCaseFixtures.VM1, vendingMachineUseCaseFixtures.VM2, vendingMachineUseCaseFixtures.VM3)
+	uc := vendingMachineUseCase.NewUseCase(vms)
+
+	_, err := uc.SelectProduct(ctx, &vendingMachineDto.SelectProductRequestDto{
+		MachineID: 4,
+		Product:   vendingMachineConstants.CoffeeType,
+	})
+
+	assert.Error(suite.T(), err)
+	assert.True(suite.T(), customError.IsNotFoundError(err))
+}
+
+func (suite *testSuite) TestSelectNotEnoughCola() {
+	ctx := context.Background()
+	var vms = make([]vendingMachineDomain.VendingMachine, 0)
+	vms = append(vms, vendingMachineUseCaseFixtures.VM4, vendingMachineUseCaseFixtures.VM5)
+	uc := vendingMachineUseCase.NewUseCase(vms)
+
+	// Attempt to select an invalid product
+	_, err := uc.SelectProduct(ctx, &vendingMachineDto.SelectProductRequestDto{
+		MachineID: vendingMachineUseCaseFixtures.VM4.ID,
+		Product:   vendingMachineConstants.ColaType,
+	})
+
+	assert.Error(suite.T(), err)
+	assert.True(suite.T(), customError.IsBadRequestError(err))
+
+	result, _ := uc.GetVendingMachineById(ctx, &vendingMachineDto.GetVendingMachineByIdRequestDto{
+		MachineID: vendingMachineUseCaseFixtures.VM4.ID,
+	})
+
+	if assert.NotNil(suite.T(), result) {
+		assert.Equal(suite.T(), vendingMachineConstants.StatusSelecting, result.Status)
+	}
+
+}
+
+func (suite *testSuite) TestSelectNotEnoughCoffee() {
+	ctx := context.Background()
+	var vms = make([]vendingMachineDomain.VendingMachine, 0)
+	vms = append(vms, vendingMachineUseCaseFixtures.VM4, vendingMachineUseCaseFixtures.VM5)
+	uc := vendingMachineUseCase.NewUseCase(vms)
+
+	// Attempt to select an invalid product
+	_, err := uc.SelectProduct(ctx, &vendingMachineDto.SelectProductRequestDto{
+		MachineID: vendingMachineUseCaseFixtures.VM5.ID,
+		Product:   vendingMachineConstants.CoffeeType,
+	})
+
+	assert.Error(suite.T(), err)
+	assert.True(suite.T(), customError.IsBadRequestError(err))
+
+	result, _ := uc.GetVendingMachineById(ctx, &vendingMachineDto.GetVendingMachineByIdRequestDto{
+		MachineID: vendingMachineUseCaseFixtures.VM5.ID,
+	})
+	if assert.NotNil(suite.T(), result) {
+		assert.Equal(suite.T(), vendingMachineConstants.StatusSelecting, result.Status)
+	}
 }
