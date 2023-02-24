b8601ae33d253a7a19e470de0fb33b4f852cfe89
saman <saman.nesary@gmail.com>
2023-02-25 00:52:20 +0330

    feat(vm): add necessary executions

--- internal/vending_machine/exception/vending_machin_exception.go
+++ internal/vending_machine/exception/vending_machin_exception.go
@@ -3,6 +3,7 @@ package vendingMachineException
 import (
 	errorConstant "github.com/samannsr/vending-machine-control-system/pkg/error/constant"
 	customErrors "github.com/samannsr/vending-machine-control-system/pkg/error/custom_error"
+	errorUtils "github.com/samannsr/vending-machine-control-system/pkg/error/error_utils"
 )
 
 func VendingMachineNotFoundExc() error {
@@ -19,4 +20,36 @@ func VendingMachineNoInventoryBadRequestExc() error {
 
 func VendingMachineInvalidProductBadRequestExc() error {
 	return customErrors.NewBadRequestError(errorConstant.ErrorLists.BadRequestError.Msg, errorConstant.ErrorLists.BadRequestError.Code, nil)
+}
+
+func GetVendingMachineInvalidIdExc() error {
+	return customErrors.NewBadRequestError(errorConstant.ErrorLists.BadRequestError.Msg, errorConstant.ErrorLists.BadRequestError.Code, nil)
+}
+
+func InsertCoinBindingExc() error {
+	return customErrors.NewInternalServerError(errorConstant.ErrorLists.InternalServerError.Msg, errorConstant.ErrorLists.InternalServerError.Code, nil)
+}
+
+func InsertCoinValidationExc(err error) error {
+	ve, ie := errorUtils.ValidationErrorHandler(err)
+	if ie != nil {
+		return ie
+	}
+
+	validationErrorCode := errorConstant.ErrorLists.ValidationError
+	return customErrors.NewValidationError(validationErrorCode.Msg, validationErrorCode.Code, ve)
+}
+
+func SelectProductBindingExc() error {
+	return customErrors.NewInternalServerError(errorConstant.ErrorLists.InternalServerError.Msg, errorConstant.ErrorLists.InternalServerError.Code, nil)
+}
+
+func SelectProductValidationExc(err error) error {
+	ve, ie := errorUtils.ValidationErrorHandler(err)
+	if ie != nil {
+		return ie
+	}
+
+	validationErrorCode := errorConstant.ErrorLists.ValidationError
+	return customErrors.NewValidationError(validationErrorCode.Msg, validationErrorCode.Code, ve)
 }
